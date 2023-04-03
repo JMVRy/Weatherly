c6b2351823b2b6dc5722d4adae31059b4d24d209
JMVRy <77213524+JMVRy@users.noreply.github.com>
2023-04-03 15:20:49 -0500

    More features
    It can now store important information from the last update, such as icon, temperature, unit, and when to next update. As such, it now will "remember" what was last shown, giving continuity. Plus, it won't send repeat requests if hasn't reached the update time.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,43 +1,100 @@
+getAllOptions().then((options) => {
+	if (options.autoUpdate)
+		fetchIt();
+	console.log(options);
+	console.log(options.lastIcon, options.lastTemp, options.lastUnit);
+	changeForecastDetails(options.lastIcon, options.lastTemp, options.lastUnit);
+});
+
 // Declare consts
-const options = getAllOptions();
 const btn = document.getElementById("request");
 const icon = document.getElementById("icon");
 const temperature = document.getElementById("temperature");
 const tempUnit = document.getElementById("unit");
-const testStorage = document.getElementById("test");
 
 // Do logic
 if (btn) btn.addEventListener("click", fetchIt);
-if (testStorage) testStorage.addEventListener("click", outputAllOptions);
 
 // Define functions
-function outputAllOptions() {
-	const opts = getAllOptions();
-	console.log(opts);
-}
-
-function getAllOptions() {
+async function getAllOptions() {
 	let opts = {
 		"autoUpdate": false,
-		"hourly": false
+		"hourly": false,
+		"lastIcon": "https://api.weather.gov/icons/land/day/sct?size=medium",
+		"lastTemp": 72,
+		"lastUnit": "F",
+		"lastUpdate": "1900-00-00T00:00:00.000Z"
 	};
 	
-	chrome.storage.local.get(["autoUpdate"]).then((value) => {opts.autoUpdate = value.autoUpdate; console.log(value)});
-	chrome.storage.local.get(["hourly"]).then((value) => {opts.hourly = value.hourly; console.log(value)});
+	await new Promise((resolve, reject) => {
+		chrome.storage.local.get(["autoUpdate"], (value) => {
+			opts.autoUpdate = value.autoUpdate;
+			console.log(value);
+			resolve();
+		});
+	});
+	await new Promise((resolve, reject) => {
+		chrome.storage.local.get(["hourly"], (value) => {
+			opts.hourly = value.hourly;
+			console.log(value);
+			resolve();
+		});
+	});
+	await new Promise((resolve, reject) => {
+		chrome.storage.local.get(["lastIcon"], (value) => {
+			opts.lastIcon = value.lastIcon;
+			console.log(value);
+			resolve();
+		});
+	});
+	await new Promise((resolve, reject) => {
+		chrome.storage.local.get(["lastTemp"], (value) => {
+			opts.lastTemp = value.lastTemp;
+			console.log(value);
+			resolve();
+		});
+	});
+	await new Promise((resolve, reject) => {
+		chrome.storage.local.get(["lastUnit"], (value) => {
+			opts.lastUnit = value.lastUnit;
+			console.log(value);
+			resolve();
+		});
+	});
+	await new Promise((resolve, reject) => {
+		chrome.storage.local.get(["lastUpdate"], (value) => {
+			opts.lastUpdate = value.lastUpdate;
+			console.log(value);
+			resolve();
+		});
+	});
 	
 	return opts;
 }
 
 async function fetchIt() {
+	let opts = await getAllOptions();
+	
+	if (new Date().getTime() - new Date(opts.lastUpdate).getTime() < 300000) {
+		console.error("Cannot request before update time (+5 mins just in case)!");
+		return;
+	}
+	
 	const url = "https://api.weather.gov/gridpoints/LOT/73,73/forecast";
 	
 	const response = await sendRequest(url);
 	
+	if (!response.properties) return;
 	changeForecastDetails(response?.properties.periods[0].icon, response?.properties.periods[0].temperature, response?.properties.periods[0].temperatureUnit);
+	
+	chrome.storage.local.set({"lastIcon": response?.properties.periods[0].icon});
+	chrome.storage.local.set({"lastTemp": response?.properties.periods[0].temperature});
+	chrome.storage.local.set({"lastUnit": response?.properties.periods[0].temperatureUnit});
+	chrome.storage.local.set({"lastUpdate": response?.properties.updateTime});
 }
 
 async function sendRequest(url) {
-	const response = await fetch(url);
+	const response = await fetch(url, {"headers": {"User-Agent": "Weatherly 1.0 Beta (github.com/JMVRy/Weatherly)"}});
 	if (response.ok) {
 		const jsonValue = response.json();
 		return Promise.resolve(jsonValue);
@@ -49,5 +106,6 @@ async function sendRequest(url) {
 function changeForecastDetails(iconUrl, temperatureValue, temperatureUnit) {
 	icon.src = iconUrl;
 	temperature.textContent = temperatureValue;
-	temperatureUnit.textContent = `º${temperatureUnit}`;
+	if (temperatureUnit == "K") tempUnit.textContent = temperatureUnit;
+	else tempUnit.textContent = `°${temperatureUnit}`;
 }
